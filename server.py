87022d89640548aafa2f4ca23d5984215baf52ba
Márton Áron <aron17marton@gmail.com>
2025-09-28 20:23:33 +0200

    feat: v1.1

--- server.py
+++ server.py
@@ -1,19 +1,45 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, send_from_directory
 import psutil
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config["DIR"] = "/home/aron/file-server-raspberry-pi/data"
 
 @app.route('/')
 def main():
     return render_template(template_name_or_list="index.html")
 
+
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        file.save(file.filename)
+        fname = secure_filename(file.filename)
+        file.save(os.path.join(app.config['DIR'], fname))
 
-        return render_template(template_name_or_list="upload.html", name=file.filename)
+        return jsonify({"info": f"'{fname}' successfully uploaded."})
+    
+
+@app.route("/get/<filename>")
+def get_file(filename):
+    return send_from_directory(app.config['DIR'], filename)
+
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    os.remove(os.path.join(app.config['DIR'], filename))
+
+    return jsonify({"info": f"'{filename}' deleted successfully."})
+    
+
+@app.route('/all', methods=["GET"])
+def get_all():
+    return jsonify({"files": get_all_files()})
+
+
+def get_all_files():
+    return os.listdir(app.config['DIR'])
 
 @app.route("/specs")
 def specs():
--- static/main.js
+++ static/main.js
@@ -1,8 +1,10 @@
-console.log("test")
-
 const btn = document.getElementById("upload-btn")
+const form = document.getElementById("upload-form")
 const input = document.getElementById("upload-input")
 const specs = document.getElementById("specs")
+const removeBtn = document.getElementById("remove-file")
+const allFilesBtn = document.getElementById("all-files-btn")
+const seeAllDiv = document.querySelector('.see-all')
 
 const getSpecs = async() => {
     try {
@@ -15,14 +17,122 @@ const getSpecs = async() => {
     }
 }
 
-btn.addEventListener("click", () => {
-    console.log("btn pressed")
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const file = input.files[0]
+    if (!file) return
+
+    const data = new FormData()
+    data.append("file", file)
+
+    try {
+        const res = await fetch("/upload", {
+            method: "POST",
+            body: data
+        })
+        const json = await res.json()
+
+        Toastify({
+            text: json.info,
+            duration: 4000,
+            gravity: "top",
+            position: "center"
+        }).showToast()
+
+        input.value = ''
+        removeBtn.hidden = true
+        btn.hidden = true
+    } catch (err) {
+        console.error(err)
+    }
 })
 
-input.addEventListener("input", () => {
-    console.log("input is inputted lol")
+input.addEventListener("change", () => {
+    if(input.value === "") {
+        btn.hidden = true
+        removeBtn.hidden = true
+    } else {
+        btn.hidden = false
+        removeBtn.hidden = false
+    }
 })
 
-// setInterval(async() => {
-//     await getSpecs()
-// }, 100_000)
+removeBtn.addEventListener("click", () => {
+    input.value = ""
+    removeBtn.hidden = true
+    btn.hidden = true
+})
+
+allFilesBtn.addEventListener("click", async() => {
+    if(allFilesBtn.innerHTML.includes('Hide')) {
+        seeAllDiv.style.display = 'none'
+        seeAllDiv.innerHTML = ''
+        allFilesBtn.innerHTML = "See all files"
+    } else {
+        const data = await fetch('https://willing-just-penguin.ngrok-free.app/all')
+        const response = await data.json()
+
+        if(response.files.length !== 0) {
+            seeAllDiv.style.cssText = `display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.1em;
+    flex-direction: column;
+    background-color: grey;
+    padding: 1em;
+    border-radius: 1em;`
+
+            for (const val of response.files) {
+                const pTag = document.createElement('p')
+                pTag.innerHTML = val
+
+                const aTag = document.createElement('a')
+                aTag.href = `/get/${val}`
+                aTag.download = true
+                aTag.innerHTML = "<button>Download</button>"
+
+                const combineTags = document.createElement('span')
+                combineTags.style.display = 'flex'
+                combineTags.style.justifyContent = 'center'
+                combineTags.style.alignItems = 'center'
+                combineTags.style.gap = '0.5em'
+
+                const buttonTagDelete = document.createElement('button')
+                buttonTagDelete.id = "delete-btn"
+                buttonTagDelete.innerHTML = "Delete"
+                buttonTagDelete.addEventListener('click', async () => {
+
+                    if (confirm(`Are you sure you want to delete '${val}'?`)) {
+                        const res = await fetch(`https://willing-just-penguin.ngrok-free.app/delete/${val}`)
+                        const resJson = await res.json()
+
+                        Toastify({
+                            text: resJson.info,
+                            duration: 5000,
+                            gravity: "top",
+                            position: "center"
+                        }).showToast();
+
+                        seeAllDiv.removeChild(combineTags)
+                    }
+                })
+
+                combineTags.appendChild(pTag)
+                combineTags.appendChild(aTag)
+                combineTags.appendChild(buttonTagDelete)
+
+                seeAllDiv.appendChild(combineTags)
+            }
+
+            allFilesBtn.innerHTML = "Hide all files"
+        } else {
+            Toastify({
+                text: "No files fetched.",
+                duration: 5000,
+                gravity: "top",
+                position: "center"
+            }).showToast();
+        }
+    }
+})
